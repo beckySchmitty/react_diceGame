13838122f7318faeea430399bdac51999fdbaf6a
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2021-03-02 22:17:32 -0500

    first draft of Dice

---
+++ src/Dice.js
@@ -0,0 +1,20 @@
+import React, {useState} from "react";
+
+const Dice = ({numDice=6, title='Main Game', maxVal=20}) => {
+    const [numbers, setNumbers] = useState(Array.from({length: numDice}));
+
+    const rollDice = () => (
+        setNumbers(numbers => (
+            numbers.map( n=> Math.floor(Math.random() * maxVal) + 1)
+        ))
+    )
+
+    return (
+        <div>
+            {numbers.map(num => <div>{num}<div/>)}
+            <button onClick={rollDice}>Roll</button>
+        </div>
+    )
+}
+
+export default Dice;
